2da08097efd48132fbe310d0fe11898341e19299
jpisoard <jpisoard@yahoo.fr>
2015-07-11 14:02:29 +0200

    testonlink

--- MyRepository/StartClass.cs
+++ MyRepository/StartClass.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyRepository.Object;
 
 namespace MyRepository
 {
@@ -13,6 +15,13 @@ namespace MyRepository
         static void Main()
         {
             Debug.WriteLine("This is a test");
+            Movie movie1 = new Movie("Batman","Warner");
+            Movie movie2 = new Movie("SuperMan", "Universal");
+            List<Movie> movieList = new List<Movie>();
+            movieList.Add(movie2);
+            movieList.Add(movie1);
+            var result = movieList.OrderBy(movie => movie.Title);
+            Debug.WriteLine(result.First());
         }
     }
 }
