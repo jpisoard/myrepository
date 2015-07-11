748025467bd7f7d0a96cb9df810f7b5ac58a2bb0
jpisoard <jpisoard@yahoo.fr>
2015-07-11 12:02:07 +0200

    test avec maxime

---
+++ MyRepository/StartClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRepository
+{
+    class StartClass
+    {
+        static void Main()
+        {
+            Debug.WriteLine("This is a test");
+        }
+    }
+}
