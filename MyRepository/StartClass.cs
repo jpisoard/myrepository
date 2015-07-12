f97eb4a3653a12fdba280ac524251e75d9a8a756
jpisoard <jpisoard@yahoo.fr>
2015-07-12 07:15:44 +0200

    push hibernate example

--- MyRepository/StartClass.cs
+++ MyRepository/StartClass.cs
@@ -6,7 +6,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MyRepository.Object;
+using MyRepository.Domain;
+using System.Configuration;
+using NHibernate.Cfg;
+using NHibernate;
+using System.Reflection;
+
 
 namespace MyRepository
 {
@@ -14,14 +19,30 @@ namespace MyRepository
     {
         static void Main()
         {
-            Debug.WriteLine("This is a test");
-            Movie movie1 = new Movie("Batman","Warner");
-            Movie movie2 = new Movie("SuperMan", "Universal");
-            List<Movie> movieList = new List<Movie>();
-            movieList.Add(movie2);
-            movieList.Add(movie1);
-            var result = movieList.OrderBy(movie => movie.Title);
-            Debug.WriteLine(result.First());
+
+            try {
+                Configuration configuration = new Configuration();
+                configuration.AddAssembly(typeof (Movie).Assembly);
+                ISessionFactory SessionFactory = configuration.BuildSessionFactory();
+                ISession session = SessionFactory.OpenSession();
+
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    Movie movie = new Movie();
+                    movie.Title = "SpiderMan";
+                    movie.Author = "Warner";
+                    session.Save(movie);
+                    transaction.Commit();
+                }
+                
+                IQuery query = session.CreateQuery("FROM Movie");
+                IList<Movie> movies = query.List<Movie>();
+                Debug.WriteLine("count" + movies.Count);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+            }
         }
     }
 }
